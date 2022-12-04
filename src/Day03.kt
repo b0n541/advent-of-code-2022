0a3cbba2e3657ced12b559d78e0e49f510e0e132
b0n541 <j@nschaefer.net>
2022-12-04 17:18:20 +0100

    Add solution for day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,21 +1,60 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            listOf(
+                line.substring(0, line.length / 2).toCharArray().toSet(),
+                line.substring(line.length / 2).toCharArray().toSet()
+            )
+        }.map { subStrings -> subStrings[0].intersect(subStrings[1]) }
+            .map { duplicatedChars -> duplicatedChars.first() }
+            .map { char -> char.code }
+            .sumOf { code ->
+                when (code) {
+                    in 97..122 -> code - 96
+                    in 65..90 -> code - 64 + 26
+                    else -> error("Unknown code $code")
+                }
+            }
     }
 
-//    fun part2(input: List<String>): Int {
-//        return input.size
-//    }
+    fun part2(input: List<String>): Int {
+        return input.chunked(3)
+            .map { chunk ->
+                listOf(
+                    chunk[0].toCharArray().toSet(),
+                    chunk[1].toCharArray().toSet(),
+                    chunk[2].toCharArray().toSet(),
+                )
+            }.map { chunkSets -> chunkSets[0].intersect(chunkSets[1]).intersect(chunkSets[2]) }
+            .map { duplicatedChars -> duplicatedChars.first() }
+            .map { char -> char.code }
+            .sumOf { code ->
+                when (code) {
+                    in 97..122 -> code - 96
+                    in 65..90 -> code - 64 + 26
+                    else -> error("Unknown code $code")
+                }
+            }
+    }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day02_test")
-    check(part1(testInput) == 15)
-//    check(part2(testInput) == 45000)
+    val testInput = readInput("Day03_test")
+    val input = readInput("Day03")
 
-    val input = readInput("Day02")
-    println("Part 1: " + part1(input))
-//    check(part1(input) == 1)
-//    println("Part 2: " + part2(input))
-//    check(part2(input) == 1)
+    val resultTest1 = part1(testInput)
+    println("Test 1: $resultTest1")
+    check(resultTest1 == 157)
+
+    val resultPart1 = part1(input)
+    println("Part 1: $resultPart1")
+    check(resultPart1 == 7831)
+
+    val resultTest2 = part2(testInput)
+    println("Test 2: $resultTest2")
+    check(resultTest2 == 70)
+
+    val resultPart2 = part2(input)
+    println("Part 2: $resultPart2")
+    check(resultPart2 == 2683)
 }
