c4bda0b77f497a9f72713bcdad3f97ffb2286bda
b0n541 <j@nschaefer.net>
2022-12-10 12:57:18 +0100

    Prepare day 5

---
+++ src/Day05.kt
@@ -0,0 +1,30 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day05_test")
+    val input = readInput("Day05")
+
+    val resultTest1 = part1(testInput)
+    println("Test 1: $resultTest1")
+//    check(resultTest1 == 2)
+//
+//    val resultPart1 = part1(input)
+//    println("Part 1: $resultPart1")
+//    check(resultPart1 == 498)
+//
+//    val resultTest2 = part2(testInput)
+//    println("Test 2: $resultTest2")
+//    check(resultTest2 == 4)
+//
+//    val resultPart2 = part2(input)
+//    println("Part 2: $resultPart2")
+//    check(resultPart2 == 859)
+}
